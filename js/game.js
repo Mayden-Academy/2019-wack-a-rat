10756c6ba460cce9745737a799c8677fb1a26df5
Audrey Hayes <audreyhayes0@gmail.com>
2019-04-02 11:21:17 +0100

    moved img to assets and deleted images dir

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var rats = document.querySelectorAll('.rat');
+var rats = document.querySelectorAll('.rat')
 
 /**
  * When you click on a rat, it disappears.
@@ -6,12 +6,12 @@ var rats = document.querySelectorAll('.rat');
  * @param {Node} rat The rat that is clicked.
  */
 function whack(rat) {
-  rat.classList.add('hidden');
+  rat.classList.add('hidden')
 }
 
 rats.forEach(function(rat) {
   rat.addEventListener('click', function() {
-    whack(rat);
-  });
-});
+    whack(rat)
+  })
+})
 
