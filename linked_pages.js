6e256a0ce2320f0756385c4088fdd95993c96f33
Alice <alice@rocketmakers.com>
2019-04-02 13:06:46 +0100

    tidied up folders and files

--- linked_pages.js
+++ linked_pages.js
@@ -1,24 +1,17 @@
-document.querySelector(".instruction_button").addEventListener('click', function () {
-    document.querySelector('.instructions-container').classList.remove('no_show')
-})
-
+var splash = document.querySelector('.splash')
+var instruction = document.querySelector('.instructions-container')
 
 document.querySelector(".instruction_button").addEventListener('click', function () {
-    document.querySelector('.splash').classList.add('no_show')
+    instruction.classList.remove('no_show')
+    splash.classList.add('no_show')
 })
-
-
 
 document.querySelector(".start_button").addEventListener('click', function () {
     document.querySelector(".gamepage").classList.remove('no_show')
-})
-
-document.querySelector(".start_button").addEventListener('click', function () {
-    document.querySelector('.splash').classList.add('no_show')
+    splash.classList.add('no_show')
 })
 
 document.querySelector(".back").addEventListener('click', function () {
-    document.querySelector('.splash').classList.remove('no_show')
-    document.querySelector('.instructions-container').classList.add('no_show')
-
+    splash.classList.remove('no_show')
+    instruction.classList.add('no_show')
 })
