214be131667316d1c94c4eb1c49d3446626649eb
Audrey Hayes <audreyhayes0@gmail.com>
2019-04-02 16:38:30 +0100

    fixed deleted code

--- js/game.js
+++ js/game.js
@@ -15,3 +15,19 @@ rats.forEach(function(rat) {
   })
 })
 
+var rats = document.querySelectorAll('.rat')
+
+/**
+ * When you click on a rat, it disappears.
+ *
+ * @param {Node} rat The rat that is clicked.
+ */
+function whack(rat) {
+  rat.classList.add('hidden')
+}
+
+rats.forEach(function(rat) {
+  rat.addEventListener('click', function() {
+    whack(rat)
+  })
+})
