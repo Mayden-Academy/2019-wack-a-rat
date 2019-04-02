ea26501d41f73865d80e62b836b39d7bd75b5264
Audrey Hayes <audreyhayes0@gmail.com>
2019-04-02 14:12:41 +0100

    changed timer back to 30

--- js/game.js
+++ js/game.js
@@ -33,7 +33,7 @@ var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 function timer() {
   count = count - 1
   if (count === 0) {
-    document.querySelector('.clock').innerHTML = 'TIMES UP!'
+    document.querySelector('.clock').innerHTML = 'TIME\'S UP!'
     hideRats(rats);
     clearInterval(counter)
   } else {
