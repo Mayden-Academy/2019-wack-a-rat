b53b824a15ec3f0f6ecb9576d96e9c7fc6ab84b3
Alessandro Corra <alecorra1980@gmail.com>
2019-04-02 13:36:12 +0100

    timer countdown created

--- js/game.js
+++ js/game.js
@@ -15,3 +15,22 @@ rats.forEach(function(rat) {
   });
 });
 
+/**
+ * Timer
+ *Timer which counts from 30 to 0 and then shows TIMES UP!
+ * @type {number}
+ */
+var count = 30;
+
+var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer() {
+  count = count - 1
+  if (count === 0) {
+    document.querySelector('.clock').innerHTML = 'TIMES UP!'
+    clearInterval(counter)
+  } else {
+    document.querySelector('.clock').innerHTML = count
+  }
+}
+
