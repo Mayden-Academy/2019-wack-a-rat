500e006dc9dcee8c898a873658bd8ed4c021644b
Alice <alice@rocketmakers.com>
2019-04-02 11:17:33 +0100

    made start and instruction button show correct pages

---
+++ linked_pages.js
@@ -0,0 +1,18 @@
+document.querySelector(".instruction_button").addEventListener('click', function () {
+    document.querySelector('.instructions-container').classList.remove('no_show')
+})
+
+
+document.querySelector(".instruction_button").addEventListener('click', function () {
+    document.querySelector('.splash').classList.add('no_show')
+})
+
+
+
+document.querySelector(".start_button").addEventListener('click', function () {
+    document.querySelector(".gamepage").classList.remove('no_show')
+})
+
+document.querySelector(".start_button").addEventListener('click', function () {
+    document.querySelector('.splash').classList.add('no_show')
+})
