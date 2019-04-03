d66e878cd08a7ca2aba032ffd3b8e035fd0e961d
Audrey Hayes <audreyhayes0@gmail.com>
2019-04-03 19:56:06 +0100

    fixed indentation

--- js/game.js
+++ js/game.js
@@ -83,7 +83,7 @@ function gameLoop() {
 function showFinalScore() {
     gameOver.classList.remove('hidden')
     gameOver.innerHTML = '<p>Game Over!</p><p class="final-score">You whacked ' + score + ' rats</p>'
-  }
+}
 
 /**
 * Hide all the rats.
